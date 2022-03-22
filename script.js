26fec0ac63d2d8faebf8f1aae07397df6aa68808
anna14b2 <anna14b2@stud.kea.dk>
2022-03-22 13:07:28 +0100

    payment

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ function navToggle() {
 }
 
 btn.addEventListener("click", navToggle);
+
+//payment
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("mobilepay-check");
+  // Get the output text
+  var text = document.getElementById("mobilepay");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  // Get the checkbox
+  var checkBox = document.getElementById("dankort");
+  // Get the output text
+  var text = document.getElementById("kort-betaling");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
